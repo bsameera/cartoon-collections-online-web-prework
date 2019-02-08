5c881977b0f1a83e4b74d0ef3734f1a7dff6da26
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-08 04:54:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(planeteer_calls)
   end 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
