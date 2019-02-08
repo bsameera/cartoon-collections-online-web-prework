065c788ace9b6920005ea11162f31db0b7acbd5f
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-08 05:01:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find 
+  array.find { |x| x==cheese_types[0] || x==cheese_types[1] || x==cheese_types[2]}
 end
